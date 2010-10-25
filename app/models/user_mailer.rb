41f0e96d77e790c5324f49926184770b6b5ab2f7
Tobias Weiß <tobias.weiss@gmail.com>
2010-10-25 15:42:51 +0200

    Minor change in user_mailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Aktivierung Ihres Benutzerkontos'
+    @subject    += ' Aktivierung Ihres Benutzerkontos'
     @body[:url]  = activate_url(user.activation_code, :host => user.site.host)
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Ihr Benutzerkonto wurde aktiviert!'
+    @subject    += ' Ihr Benutzerkonto wurde aktiviert!'
     @body[:url]  = root_url(:host => user.site.host)
   end
   
@@ -15,7 +15,7 @@ class UserMailer < ActionMailer::Base
     def setup_email(user)
       @recipients  = "#{user.email}"
       @from        = "noreply@gute-arbeit-alleinerziehende.de"
-      @subject     = "[Gute Arbeit] "
+      @subject     = "[Gute Arbeit]"
       @sent_on     = Time.now
       @body[:user] = user
     end
