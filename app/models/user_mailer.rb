cd5544c4c4f730d72d933162500da3d924361f98
Tobias Weiß <tobias.weiss@gmail.com>
2010-10-25 10:35:21 +0200

    Updated activation and signup e-mails

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,21 +1,21 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
+    @subject    += 'Aktivierung Ihres Benutzerkontos'
     @body[:url]  = activate_url(user.activation_code, :host => user.site.host)
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
+    @subject    += 'Ihr Benutzerkonto wurde aktiviert!'
     @body[:url]  = root_url(:host => user.site.host)
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "noreply@gute-arbeit-alleinerziehende.de"
+      @subject     = "[Gute Arbeit]"
       @sent_on     = Time.now
       @body[:user] = user
     end
